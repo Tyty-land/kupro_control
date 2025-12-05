8fea22fee119c3e8c5687cd4a057cdcb5265226b
papa_A <ss-200909@mail.ru>
2025-12-05 19:20:03 +0400

    Объявление классов ProcessInfo.cs для обоих проектов, также добавление класса-утилиты LoggerHelper.cs также для обоих проектов

--- src/kupro_control/child_monitor/Program.cs
+++ src/kupro_control/child_monitor/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
+using child_monitor.Utils;
 
 namespace child_monitor
 {
